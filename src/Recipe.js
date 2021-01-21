034597135a2153e9aca642adfda9cc1c5dc377de
bruno talarico <bruno.talarico@gmail.com>
2021-01-21 13:41:37 +0000

    app done, needs styling

---
+++ src/Recipe.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import style from './recipe.module.css';
+const Recipe = ({title, calories, image, ingredients}) => {
+  return(
+    <div className={style.recipe}>
+      <h2>{title}</h2>
+      <h3>Ingredients:</h3>
+      <ol>
+        {ingredients.map(ingredient => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ol>
+      <p>Calories: {Math.floor(calories)}</p>
+      <img className={style.image} src={image} alt={title} />
+    </div>
+  );
+}
+
+export default Recipe;
